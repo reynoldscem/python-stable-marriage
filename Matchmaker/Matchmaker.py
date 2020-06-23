1d4de35bb3d5ca8c336921fabd60e6cf22f47ddf
Charles EM Reynolds <reynoldscem@gmail.com>
2020-06-23 14:44:44 +0100

    Refactoring to use instance variables not marshsal around arguments

--- Matchmaker/Matchmaker.py
+++ Matchmaker/Matchmaker.py
@@ -1,34 +1,43 @@
 class Matchmaker:
-    def __propose(self, men, women, measure, prefs):
-        for i, man in enumerate(men):
+    def __init__(self, men, women, measure):
+        self.men = [self._make_man_container(man) for man in men]
+        self.women = women
+        self.measure = measure
+
+        assert len(women) >= len(men), (
+            "Must have more (or the same) women than men."
+        )
+
+    def __propose(self, prefs):
+        for i, man in enumerate(self.men):
             if man['proposed_to']:
                 continue
             crush = None
             crush_score = None
-            for j, woman in enumerate(women):
+            for j, woman in enumerate(self.women):
                 if j in man['exes']:
                     continue
-                prefs[i, j] = prefs.get((i, j), measure(i, j))
+                prefs[i, j] = prefs.get((i, j), self._measure(i, j))
                 if crush_score is None or prefs[i, j] > crush_score:
                     crush_score = prefs[i, j]
                     crush = j
             man['proposed_to'] = crush
 
-    def __select(self, men, women, measure, prefs):
+    def __select(self, prefs):
         refusation_count = sum([
-            1 for man in men if man['proposed_to'] is None
+            1 for man in self.men if man['proposed_to'] is None
         ])
-        for j, woman in enumerate(women):
+        for j, woman in enumerate(self.women):
             crush = None
             crush_score = None
-            for i, man in enumerate(men):
+            for i, man in enumerate(self.men):
                 if man['proposed_to'] != j:
                     continue
-                prefs[i, j] = prefs.get((i, j), measure(i, j))
+                prefs[i, j] = prefs.get((i, j), self._measure(i, j))
                 if crush_score is None or prefs[i, j] > crush_score:
                     if crush is not None:
-                        men[crush]['proposed_to'] = None
-                        men[crush]['exes'].append(j)
+                        self.men[crush]['proposed_to'] = None
+                        self.men[crush]['exes'].append(j)
                         refusation_count += 1
                     crush_score = prefs[i, j]
                     crush = i
@@ -39,14 +48,17 @@ class Matchmaker:
         return refusation_count
 
     @staticmethod
-    def make_man_container(man):
+    def _make_man_container(man):
         return {
             'soul': man,
             'proposed_to': None,
             'exes': []
         }
 
-    def marry(self, men, women, measure):
+    def _measure(self, i, j):
+        return self.measure(self.men[i]['soul'], self.women[j])
+
+    def marry(self):
         '''
         Run Gale-Shapley algorithm on men and women lists
 
@@ -55,30 +67,26 @@ class Matchmaker:
 
         Returns : list of tuples [(man, woman, couple_score), (...) ...]
         '''
-        men = [self.make_man_container(man) for man in men]
         self.prefs = {}
-
-        def _measure(i, j):
-            return measure(men[i]['soul'], women[j])
 
         while True:
             unmarried_men = [
-                man for man in men
+                man for man in self.men
                 if man['proposed_to'] is None
             ]
             if not unmarried_men:
                 break
-            self.__propose(men, women, _measure, self.prefs)
-            refusation_count = self.__select(men, women, _measure, self.prefs)
+            self.__propose(self.prefs)
+            refusation_count = self.__select(self.prefs)
             if not refusation_count:
                 break
 
         def _solution_tuple(i, man):
             name = man['soul']
             proposed_to = man['proposed_to']
-            bride = women[proposed_to]
+            bride = self.women[proposed_to]
             preference = self.prefs[i, proposed_to]
 
             return (name, bride, preference)
 
-        return [_solution_tuple(index, man) for index, man in enumerate(men)]
+        return [_solution_tuple(index, man) for index, man in enumerate(self.men)]
